711c3bb5416f43693623fdbbf5a85ddc05188d40
Tanmay <bindaltanmay1996@gmail.com>
2024-05-10 17:55:41 +0530

    feat: Add redirect for /api/* to news API endpoint

--- src/api/newa-api/index.ts
+++ src/api/newa-api/index.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const apiKey = import.meta.env.VITE_NEWS_API_KEY;
-const baseURL = 'https://newsapi.org/v2';
+const apiKey =
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    import.meta.env.VITE_NEWS_API_KEY ?? '994e4e4674f948b3801f497226dcb2df';
+
+// const baseURL = 'https://newsapi.org/v2';
+const baseURL = '/api';
 
 export const newsAPIInstance = axios.create({
     baseURL,
