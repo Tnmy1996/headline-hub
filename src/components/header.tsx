057a263147bb330ee2f413294636bc0d047fefee
Tanmay <bindaltanmay1996@gmail.com>
2024-05-10 23:30:17 +0530

    fix heading

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
             <div className='flex h-16 w-full items-center justify-between gap-4 border-b px-4 md:px-6'>
                 <div></div>
                 <h2 className='absolute left-1/2 -translate-x-1/2 font-bold tracking-tight sm:text-2xl md:text-3xl'>
-                    <Link to='/'>Heading Hub</Link>
+                    <Link to='/'>Headline Hub</Link>
                 </h2>
                 <div className='flex items-center space-x-4'>
                     <ModeToggle />
