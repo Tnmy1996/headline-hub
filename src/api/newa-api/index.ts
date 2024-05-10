2dc5618ec2816c1cbb03606a59f7687367e45bc4
Tanmay <bindaltanmay1996@gmail.com>
2024-05-10 18:16:39 +0530

    chore: Update netlify.toml redirects and news API endpoint URL

--- src/api/newa-api/index.ts
+++ src/api/newa-api/index.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-const apiKey =
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    import.meta.env.VITE_NEWS_API_KEY ?? '994e4e4674f948b3801f497226dcb2df';
+const apiKey = import.meta.env.VITE_NEWS_API_KEY;
 
-// const baseURL = 'https://newsapi.org/v2';
-const baseURL = '/api';
+const baseURL = 'https://newsapi.org/v2';
 
 export const newsAPIInstance = axios.create({
     baseURL,
